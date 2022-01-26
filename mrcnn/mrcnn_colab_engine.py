09a7320c8b4bc740a4a3ee28069d4430cb8abeae
MarcoTorresValenzuela <93018412+MarcoTorresValenzuela@users.noreply.github.com>
2022-01-26 20:35:21 -0300

    Update mrcnn_colab_engine.py

--- mrcnn/mrcnn_colab_engine.py
+++ mrcnn/mrcnn_colab_engine.py
@@ -20,7 +20,7 @@ class InferenceConfig(Config):
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
-    NUM_CLASSES = 34
+    NUM_CLASSES = 37
     NAME = 'coco'
 
 def get_mask_contours(mask):
