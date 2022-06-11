b8e83de09d9b39ff01cb3d69062fd360368076cf
MarcoTorresValenzuela <93018412+MarcoTorresValenzuela@users.noreply.github.com>
2022-06-11 18:38:42 -0400

    Update Function_faster.py

--- mrcnn/Function_faster.py
+++ mrcnn/Function_faster.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
 def deteccion_imagen(image_path,PATH_TO_CKPT):
   # Valores de la red neuronal propia
   num_classes = 1 # numero de clases de la red
